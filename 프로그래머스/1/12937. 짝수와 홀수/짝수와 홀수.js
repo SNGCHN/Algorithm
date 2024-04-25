783ae98229b913d327d46e705c207f05696a51d1
Seongchan Lee <110635641+SNGCHN@users.noreply.github.com>
2024-04-25 12:49:16 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,11 @@
+function solution(num) {
+    var answer = '';
+    
+    if (num % 2 === 0) {
+        return num = "Even"
+    } else {
+        return num = "Odd"
+    }
+    
+    return answer;
+}
