bf292d5a119f9275eca328185ddf9d84a7fdd72c
Seongchan Lee <110635641+SNGCHN@users.noreply.github.com>
2026-02-27 18:28:05 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 104 ms, Memory: 9504 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -0,0 +1,26 @@
+const readline = require("readline");
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const __lines = [];
+rl.on("line", (line) => __lines.push(line));
+rl.on("close", () => {
+  const input = __lines.join('\n').trim();
+
+  const solution = (input) => {
+    const N = parseInt(input, 10);
+    let result = "";
+
+    for (let i = 1; i <= N; i++) {
+      result += " ".repeat(N - i) + "*".repeat(i);
+      if (i !== N) result += "\n";
+    }
+
+    return result;
+  };
+
+  const __result = solution(input);
+  if (__result !== undefined) {
+    if (Array.isArray(__result)) console.log(__result.join('\n'));
+    else console.log(String(__result));
+  }
+  process.exit();
+});
